f12d8e271c36ad23b37d1d61ebfbe9ef9fe3c903
Rishabhs1711 <rishabhs1711@gmail.com>
2018-01-30 00:03:01 -0800

    ajeeb branch
    in case needed

---
+++ Platformer 3D/Assets/Custom Scripts/PlayerMotor.cs
@@ -0,0 +1,62 @@
+﻿ using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour {
+
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+    private Vector3 cameraRotation = Vector3.zero;
+
+    [SerializeField]
+    private Camera cam;
+
+    private Rigidbody rb;
+
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+
+    //gets a movement vector
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    //gets a rotation vector
+    public void rotate(Vector3 _rotation)
+    {
+        rotation = _rotation;
+    }
+
+    //gets a camera rotation vector
+    public void rotateCamera(Vector3 _cameraRotation)
+    {
+       cameraRotation = _cameraRotation;
+    }
+
+    //run every physics iteration
+    void FixedUpdate () {
+        PerformMovement();
+        PerformRotation();
+	}
+
+    //perform movement based on velocity variable
+    void PerformMovement()
+    {
+        if (velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position - velocity * Time.fixedDeltaTime);
+        }
+    }
+
+    void PerformRotation()
+    {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+        if (cam != null)
+        {
+            cam.transform.Rotate(-cameraRotation);
+        }
+    }
+}
---
+++ Platformer 3D/Assets/Custom Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿ using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField]
+    private float speed = 5f;
+    [SerializeField]
+    private float LookSensitivity = 3f;
+
+    private PlayerMotor Motor;
+
+	// Use this for initialization
+	void Start () {
+        Motor = GetComponent<PlayerMotor>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //calculate velocity magnitude and direction
+        float xMov = Input.GetAxisRaw("Horizontal");
+        float zMov = Input.GetAxisRaw("Vertical");
+
+        //horizontal and vertical vectors
+        Vector3 MovHorizontal = transform.right * xMov;
+        Vector3 MovVertical = transform.forward * zMov;
+
+        //calculate final velocity vector 
+        Vector3 _velocity = (MovHorizontal + MovVertical).normalized * speed;
+
+        //apply movement
+        Motor.Move(_velocity);
+
+        //calculate rotation as a 3D vector; turning around
+        float _yRot = Input.GetAxisRaw("Mouse X");
+
+        Vector3 _rotation = new Vector3(0f, _yRot , 0f) * LookSensitivity;
+
+        //apply player rotation
+        Motor.rotate(_rotation);
+
+        float _xRot = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 _cameraRotation = new Vector3(0f, _xRot, 0f) * LookSensitivity;
+
+        Motor.rotateCamera(_cameraRotation);
+
+
+    }
+}
